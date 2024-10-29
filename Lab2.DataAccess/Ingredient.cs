5d98600ab95a2e4e2c0698340338669bdcc51e32
Alice <gucanea@gmail.com>
2024-10-29 23:27:52 +0200

    Add project files.

---
+++ Lab2.DataAccess/Ingredient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.DataAccess
+{
+    public class Ingredient
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        public float Amount { get; set; }
+
+        [Required]
+        [StringLength(10)]
+        public string Units { get; set; }
+
+        public Recipe Recipe { get; set; }
+    }
+}
+
---
+++ Lab2.DataAccess/Recipe.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2.DataAccess
+{
+    public class Recipe
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string DishName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Category { get; set; }
+
+        [Required] 
+        [StringLength(20)]
+        public string PrepTime { get; set; }
+
+        public List<Ingredient> Ingredients { get; set; } = new ();
+    }
+}
